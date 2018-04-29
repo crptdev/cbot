ab4be1c651b67360daf9a8c157c3959802e03ab0
crptdev <36066435+crptdev@users.noreply.github.com>
2018-04-29 13:15:41 +0200

    Update rmq.modmgo.py

--- rmq.modmgo.py
+++ rmq.modmgo.py
@@ -9,10 +9,14 @@ import json
 import csv
 import telegram
 
+# fichier ini pour se connecter à la base Mongo du type :
+#[mgo]
+# Server=mango-36.atlantis.io:27017
+# Base=clcw
+# Coll=tradescol
 
-RMQServer='seabunny.atlantis.io'
-
-def f_put_tlg(bot, chat_id, msg):
+#
+def f_put_mgo(ot, chat_id, msg):
     msg =''
     for row in entetes:
             msg = msg + '<b>' + row + '</b>' + ','
@@ -30,29 +34,39 @@ def f_Load_Conf(Fic):
 	conf =configparser.ConfigParser ()
 	conf .readfp (open (Fic))
 	#logger.info('Lecture du fichier %s', Fic)
-	if conf.has_option('tlg', 'Tlgkey'):
-		Tlgkey = conf.get ('tlg', 'Tlgkey')
-		chat_id = conf.get ('tlg', 'chat_id')
+	if conf.has_option('mgo', 'Base'):
+		Server = conf.get ('mgo', 'Server')
+		Base = conf.get ('mgo', 'Base')
+		Coll = conf.get ('mgo', 'Coll')
 	else:
-		Tlgkey= ''
-		chat_id= ''
-	return Tlgkey, chat_id
+		Server = ''
+		Base = ''
+		Coll = ''
+	return Server,Base,Coll
+
+def get_base(Server,Coll):
+    from pymongo import MongoClient
+    client = MongoClient(Server)
+    db = client[Coll]
+    return db
 
 def main():
   
-  Tlgkey, chat_id = f_Load_Conf(sys.argv[0][:-3]  + '.ini')
-  bot = f_init_tlg(Tlgkey)
+  Server, Base, Coll = f_Load_Conf(sys.argv[0][:-3]  + '.ini')
+  print(Server + ' ' + Base + ' ' + Coll)
+  db = get_base(Server,Coll)
    
   connection = pika.BlockingConnection(pika.ConnectionParameters(host=RMQServer))
   channel = connection.channel()
-  channel.queue_declare(queue='tlg')
+  channel.queue_declare(queue='mgo')
   
   def callback(ch, method, properties, body):
-    bot.send_message(chat_id=chat_id, text=str(body), parse_mode=telegram.ParseMode.HTML)
+    # bot.send_message(chat_id=chat_id, text=str(body), parse_mode=telegram.ParseMode.HTML)
     print("Message Telegram" % body)
+    print(body)
     
 
-  channel.basic_consume(callback, queue='tlg', no_ack=True)
+  channel.basic_consume(callback, queue='mgo', no_ack=True)
 
   print(' [*] Waiting for messages. To exit press CTRL+C')
   channel.start_consuming()
